1830ff8acfc85d214c716f11cab16a44160e6223
Shaan Sundar <shaan.idtindia@gmail.com>
2021-06-29 11:54:30 +0530

    Metamask support added, also cleaned up modal

--- components/PageHeader/PageHeader.js
+++ components/PageHeader/PageHeader.js
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
-import {Dropdown, Menu, Container, Image} from "semantic-ui-react";
+import {Dropdown, Menu, Container, Image, Button, Modal, Header, Icon} from "semantic-ui-react";
 import React from "react";
-import PopUpWindow from "../PopUpWindow/PopUpWindow";
+import UploadForm from "@/components/UploadForm/UploadForm";
+import MetaMask from "@/components/Wallets/MetaMask";
 
-const PageHeader = () => (
+const PageHeader = () => {const [open, setOpen] = React.useState(false); return(
 <Menu fixed="top" borderless>
       <Container>
         <Menu.Item as="a" header>
@@ -13,8 +14,21 @@ const PageHeader = () => (
         <Menu.Item as="a">Contact Us</Menu.Item>        
       </Container>
       
-      <Menu.Item as="a" align="right">Connect Wallet</Menu.Item>
-      <PopUpWindow/>
-    </Menu>
-)
+      <Menu.Item as="a" align="right" onClick={()=>MetaMask()}>Connect Wallet</Menu.Item>
+      <Modal
+        basic
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        size='small'
+        trigger={<Menu.Item as="a" align="right">Upload</Menu.Item>}
+      >
+        <Modal.Content>
+          <UploadForm />
+        </Modal.Content>
+      </Modal>
+      </Menu>
+)}
 export default PageHeader
+
+// <Button color="teal" onClick={()=>PopUpWindow()}> Upload</Button>
