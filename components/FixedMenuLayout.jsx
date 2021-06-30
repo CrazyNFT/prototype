68357c68ed70448625a0790b13f702db49010081
Karan Yuvraj Singh <karan124d@gmail.com>
2021-06-30 18:08:39 +0530

    changes
    - Removed header
    - Filename should have same name as class or functional component

--- components/FixedMenuLayout.jsx
+++ components/FixedMenuLayout.jsx
@@ -15,37 +15,6 @@ import Card2 from "./Card2";
 
 const FixedMenuLayout = () => (
   <div>
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item as="a" header>
-          <Image
-            size="mini"
-            src="https://source.unsplash.com/random"
-            style={{ marginRight: "1.5em" }}
-          />
-          Project Name
-        </Menu.Item>
-        <Menu.Item as="a">Home</Menu.Item>
-
-        <Dropdown item simple text="Dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className="dropdown icon" />
-              <span className="text">Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
 
     <Container text style={{ marginTop: "7em", textAlign: "center" }}>
       <Card1 />
