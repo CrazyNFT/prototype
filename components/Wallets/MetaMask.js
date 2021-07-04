2a6f22f4405b1d3234d0a0f73a3967d1464d9047
Shaan Sundar <shaan.idtindia@gmail.com>
2021-07-04 13:55:24 +0530

    Disables SideNav for time being

--- components/Wallets/MetaMask.js
+++ components/Wallets/MetaMask.js
@@ -1,9 +1,11 @@
 import Web3 from "web3";
-
+let connected=false
 const MetaMask = async () => {
+  
   if (window.ethereum) {
     await window.ethereum.send('eth_requestAccounts');
     window.web3 = new Web3(window.ethereum);
+    connected = true;
     return true;
   }
   return false;
