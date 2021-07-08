c4d7b0a7afbd7f4eb926913d14d9d18e0cd0d018
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-07-09 00:29:33 +0530

    Create Comp1.js

---
+++ landingpage/Comp1.js
@@ -0,0 +1,33 @@
+const Comp1 = () => {
+    return ( 
+        <>
+                 <div>
+                     <div className = "container">
+                         <div className="heading">
+                         <p className = "Head1">Why CrazyNFT ?</p>
+                         </div>
+                         <div className = "container">
+
+                        
+                         <div>
+                             <p className ="para">
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/> 
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/>
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/>
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/>
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/>
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/>
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation <br/>
+
+                             </p>
+                         </div>
+                         </div>
+
+                     </div>
+                 </div>
+        
+        </>
+     );
+}
+ 
+export default Comp1;
