56f18006aa28780fe9862a4352cbf6f37a65e50b
Shaan Sundar <shaan.idtindia@gmail.com>
2021-08-12 11:10:55 +0530

    updates_check

--- components/Retrieve/TableMaker.js
+++ components/Retrieve/TableMaker.js
@@ -1,12 +1,39 @@
 import React from "react";
 import { Container, Grid } from "semantic-ui-react";
 import NFTCard from "./NFTCard";
-import contacts from "../extras/contacts";
+
+const axios = require('axios');
+let heroku = () => axios.get('https://crazynft.herokuapp.com/getall.php')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+    heroku=response;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+  });
+
+
+
+const response = async () => await fetch("");
+// const db = async () => await response.json();
+// const uri = async () => await fetch(`ipfs://${db.uri}`);
+// const uridb = async () => await db.json();
+// const pic = async () => await fetch(`ipfs://${uridb.nftFile}`);
+// const picdb = async () => await pic.json();
+
+
 
 const TableMaker = () => {
-  return (
+  // response();
+  // console.log(contacts);
+  console.log(heroku());
+  return (    
     <div style={{ margin: "84px 320px" }}>
-      <Grid columns={3} relaxed style={{ margin: "auto" }}>
+      {/* <Grid columns={3} relaxed style={{ margin: "auto" }}>
         <Grid.Row>
           <Grid.Column>
             <NFTCard
@@ -53,8 +80,8 @@ const TableMaker = () => {
             />
           </Grid.Column>
         </Grid.Row>
-      </Grid>
+      </Grid> */}
     </div>
-  );
+   );
 };
 export default TableMaker;
