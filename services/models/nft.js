53d6e55c73148810877eed847c730bcb78414ada
Shivansh Goel <shivanshgoel21@gmail.com>
2021-08-14 13:04:13 +0530

    nft.js: Implement getAll method

--- services/models/nft.js
+++ services/models/nft.js
@@ -21,7 +21,19 @@ class Nft {
         } catch (err) {
             return err;
         }
+    }
 
+    async getAll() {
+        try{
+            const snapShot = await db.collection(this.nftCollection).get();
+            const collection = []
+            snapShot.forEach(doc => {
+                collection.push(doc.data())
+            })
+            return collection;
+        } catch(err){
+            return err
+        }
     }
 
 }
