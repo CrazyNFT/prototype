ee5ae808b98182eea576c541c3132c72fc6e282e
Shivansh Goel <shivanshgoel21@gmail.com>
2021-08-14 13:04:38 +0530

    TableMaker.js: Integrate getAll method from NFT model

--- components/Retrieve/TableMaker.js
+++ components/Retrieve/TableMaker.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Grid } from "semantic-ui-react";
 import NFTCard from "./NFTCard";
 
+import NFT from '../../services/models/nft'
+
 const axios = require('axios');
-let heroku = () => axios.get('https://crazynft.herokuapp.com/getall.php')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-    heroku=response;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-  });
+// let heroku = () => axios.get('https://crazynft.herokuapp.com/getall.php')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//     heroku=response;
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//   });
 
 
 
@@ -29,27 +31,43 @@ const response = async () => await fetch("");
 
 const TableMaker = () => {
 
+  const [nftData, setNftData] = useState([])
+
+  useEffect(async () => {
+    try {
+      let nft = new NFT()
+      let data = await nft.getAll()
+      console.log('NFT Data ', data)
+      if (data) {
+        setNftData(data)
+      }
+    } catch (err) {
+      alert('Error! ' + err)
+    }
+  }, [])
+
   // response();
   // console.log(contacts);
-  console.log(heroku());
-  return (    
+  // console.log(heroku());
+  return (
     <div style={{ margin: "84px 320px" }}>
-      {/* <Grid columns={3} relaxed style={{ margin: "auto" }}>
-        <Grid.Row>
-          <Grid.Column>
-            <NFTCard
-              name={ele.name}
-              img={ele.imgURL}
-              details={ele.details}
-            />
-          </Grid.Column>
-            )
-          
-          })
-        }
-        </Grid.Row>
-      </Grid> */}
+      {
+        nftData && nftData.map((data) => (
+          <Grid columns={3} relaxed style={{ margin: "auto" }}>
+            <Grid.Row>
+              <Grid.Column>
+                <NFTCard
+                  name={data.nftName}
+                  img={'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80'} // test
+                  details={data.nftDescription}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        ))
+      }
+
     </div>
-   );
+  );
 };
 export default TableMaker;
